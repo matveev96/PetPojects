ac9df4eb35237e0f71769c8c42eef259d54fa227
matveev96 <sasha.matveev1996@icloud.com>
2024-08-22 17:48:20 +0300

    feat: add js in pets-page burger-menu

--- shelter/pages/pets/src/index.js
+++ shelter/pages/pets/src/index.js
@@ -1,1 +1,32 @@
-console.log('100 баллов\nВсе пункты выполнены в полном объеме\n https://rolling-scopes-school.github.io/checklist/');
+//Burger menu
+
+const humburger = document.querySelector(".humburger");
+
+const offScreenMenu = document.querySelector(".header__mobile-menu");
+
+const backgroundScreenMenu = document.querySelector(".background_screen");
+
+const body = document.querySelector("body");
+
+const listItems = document.querySelector(".header__list");
+
+humburger.addEventListener("click", () => {
+  humburger.classList.toggle("active");
+  offScreenMenu.classList.toggle("active");
+  backgroundScreenMenu.classList.toggle("active");
+  body.classList.toggle("menu-opend");
+});
+
+backgroundScreenMenu.addEventListener("click", () => {
+    humburger.classList.remove("active");
+    offScreenMenu.classList.remove("active");
+    backgroundScreenMenu.classList.remove("active");
+    body.classList.remove("menu-opend");
+});
+
+listItems.addEventListener("click", () => {
+    humburger.classList.remove("active");
+    offScreenMenu.classList.remove("active");
+    backgroundScreenMenu.classList.remove("active");
+    body.classList.remove("menu-opend");
+});
