01137e2febaa02915baab5062bb556cbce7d372f
matveev96 <sasha.matveev1996@icloud.com>
2024-09-16 15:24:43 +0300

    refactor: transfer shelter files in separate folder

--- webpack.config.js
+++
@@ -1,8 +0,0 @@
-const path = require('path');
-module.exports = {
-  entry: './shelter/pages/main/src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
-  }
-};
