745f16f2f4ab0d4fffe75820ec8328734083503e
matveev96 <sasha.matveev1996@icloud.com>
2024-10-01 09:35:26 +0300

    feat: add Game over text

--- js30-random-game/index.js
+++ js30-random-game/index.js
@@ -13,9 +13,20 @@ fg.src = "./assets/img/flappy_bird_fg.png";
 pipeUp.src = "./assets/img/flappy_bird_pipeUp.png";
 pipeBottom.src = "./assets/img/flappy_bird_pipeBottom.png";
 
+let flyAudio = new Audio();
+let scoreAudio = new Audio();
+let gameAudio = new Audio();
+let failAudio = new Audio();
+
+flyAudio.src = "./assets/audio/fly-button.mp3";
+scoreAudio.src = "./assets/audio/score.mp3";
+gameAudio.src = "./assets/audio/continue-game.mp3";
+failAudio.src = "./assets/audio/fail-kurwa.mp3";
+
 document.addEventListener("keydown", moveUp);
 function moveUp() {
     yPos -= 40;
+    flyAudio.play();
 }
 
 let pipe = [];
@@ -30,7 +41,11 @@ let xPos = 10;
 let yPos = 150;
 let grav = 1.3;
 
+let score = 0;
+
 function draw() {
+
+    gameAudio.play();
     ctx.drawImage(bg, 0, 0);
 
     for(let i = 0; i < pipe.length; i++) {
@@ -49,15 +64,31 @@ function draw() {
             && xPos <= pipe[i].x + pipeUp.width
             && (yPos <= pipe[i].y + pipeUp.height
             || yPos + bird.height >= pipe[i].y + pipeUp.height + gap) || yPos + bird.height >= cvs.height - fg.height) {
-            location.reload(); // Перезагрузка страницы
-            }
-    }
+            
+            failAudio.play();
 
+            ctx.fillStyle = "#000";
+            ctx.font = "24px Verdana";
+            ctx.fillText("Game Over", 80, cvs.height / 2);
+
+            location.reload(); // Перезагрузка страницы
+        }
+
+        if(pipe[i].x == 5) {
+            score++;
+            scoreAudio.play();
+        }
+    }
     
     ctx.drawImage(fg, 0, cvs.height - fg.height);
-
     ctx.drawImage(bird, xPos, yPos);
+
     yPos += grav;
+
+    ctx.fillStyle = "#000";
+    ctx.font = "24px Verdana";
+    ctx.fillText("Score: " + score, 10, cvs.height - 20);
+
     requestAnimationFrame(draw);
 }
 
