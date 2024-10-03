d08f5e875ef9ef455dbee513373b22d458d30dd9
matveev96 <sasha.matveev1996@icloud.com>
2024-10-03 18:05:26 +0300

    feat: add gameTime, countOfAttempts, footer and bg.img

--- js30-random-game/index.js
+++ js30-random-game/index.js
@@ -46,25 +46,14 @@ let yPos = 150;
 let grav = 1.3;
 let score = 0;
 let gameOver = false;
+let attempts = 0;
 
 const canvasDiv = document.querySelector(".canvas_div");
-
-const restartButton = document.createElement("button");
-restartButton.className = "start_button";
-restartButton.innerHTML = "Start";
-restartButton.style.position = "absolute";
-restartButton.style.top = "20%";
-restartButton.style.left = "50%";
-restartButton.style.transform = "translate(-50%, -50%)";
-restartButton.style.padding = "10px 20px";
-restartButton.style.fontSize = "24px";
-restartButton.style.display = "block";
-canvasDiv.appendChild(restartButton);
 
 const gameOverText = document.createElement("p");
 gameOverText.className = "welcome_text";
 gameOverText.style.position = "absolute";
-gameOverText.style.top = "25%";
+gameOverText.style.top = "30%";
 gameOverText.style.left = "8%";
 gameOverText.style.width = "250px";
 gameOverText.style.fontSize = "24px";
@@ -75,20 +64,32 @@ const textWelcome = document.createElement("p");
 textWelcome.className = "welcome_text";
 textWelcome.innerHTML = "Welcome to Flappy Bird!";
 textWelcome.style.position = "absolute";
-textWelcome.style.top = "25%";
+textWelcome.style.top = "5%";
 textWelcome.style.left = "5%";
 textWelcome.style.width = "250px";
 textWelcome.style.fontSize = "24px";
 textWelcome.style.display = "block";
 canvasDiv.appendChild(textWelcome);
 
+const restartButton = document.createElement("button");
+restartButton.className = "start_button";
+restartButton.innerHTML = "Start";
+restartButton.style.position = "absolute";
+restartButton.style.top = "50%";
+restartButton.style.left = "50%";
+restartButton.style.transform = "translate(-50%, -50%)";
+restartButton.style.padding = "10px 20px";
+restartButton.style.fontSize = "24px";
+restartButton.style.display = "block";
+canvasDiv.appendChild(restartButton);
+
 const description = document.createElement("p");
 description.className = "description_text";
-description.innerHTML = "Your goal is 100 points. Press Start and enjoy your fly :)";
+description.innerHTML = "Score 20 points to Win";
 description.style.position = "absolute";
-description.style.top = "72%";
+description.style.top = "80%";
 description.style.left = "5%";
-description.style.width = "250px";
+description.style.width = "260px";
 description.style.fontSize = "15px";
 description.style.display = "block";
 canvasDiv.appendChild(description);
@@ -97,17 +98,26 @@ const yourScore = document.createElement("p");
 yourScore.className = "your_score";
 yourScore.innerHTML = "Last 10 results:";
 yourScore.style.position = "absolute";
-yourScore.style.top = "40%";
+yourScore.style.top = "45%";
 yourScore.style.left = "7%";
 yourScore.style.width = "250px";
 yourScore.style.display = "block";
 canvasDiv.appendChild(yourScore);
 
+const countAttempts = document.createElement("p");
+countAttempts.className = "attempts";
+countAttempts.style.position = "absolute";
+countAttempts.style.top = "60%";
+countAttempts.style.left = "7%";
+countAttempts.style.width = "260px";
+countAttempts.style.display = "block";
+canvasDiv.appendChild(countAttempts);
+
 const listScore = document.createElement("div");
 listScore.className = "list_score";
 listScore.innerHTML = "";
 listScore.style.position = "absolute";
-listScore.style.top = "48%";
+listScore.style.top = "52%";
 listScore.style.width = "250px";
 listScore.style.display = "block";
 canvasDiv.appendChild(listScore);
@@ -118,6 +128,7 @@ function startGame() {
   ctx.drawImage(bg, 0, 0);
   yourScore.style.display = "none";
   gameOverText.style.display = "none";
+  timerDiv.style.display = "none";
 }
 //RESTART GAME
 function restartGame() {
@@ -139,23 +150,35 @@ let scoreArr = [];
 function scoreControl(score) {
   let scoreAcc = "";
   let scoreItem = `Score: ${score}`;
+  let scoreStyle = "";
+
+  if (score >= 2) {
+    scoreStyle = "win_item";
+  }
+
   if (scoreArr.length >= 10) {
     scoreArr = [scoreItem];
-    listScore.innerHTML = `<li class="list_item">${scoreArr.length} ${scoreArr[0]}</li>`;
+    listScore.innerHTML = `<li class="list_item ${scoreStyle}">${scoreArr.length} ${scoreArr[0]}</li>`;
   } else {
     scoreArr.push(scoreItem);
   }
 
-  for(let i = 0; i < scoreArr.length; i++) {
-    scoreAcc += `<li class="list_item">${i + 1} ${scoreArr[i]}</li>`
+  for (let i = 0; i < scoreArr.length; i++) {
+    scoreAcc += `<li class="list_item ${scoreStyle}">${i + 1} ${
+      scoreArr[i]
+    }</li>`;
     listScore.innerHTML = `${scoreAcc}`;
   }
 }
+
+
 //PLAY ANIMATION
 function draw() {
   if (gameOver) {
     return;
   }
+
+  startTimer();
 
   startGameAudio.pause();
   textWelcome.style.display = "none";
@@ -164,6 +187,8 @@ function draw() {
   listScore.style.display = "none";
   yourScore.style.display = "none";
   gameOverText.style.display = "none";
+  timerDiv.style.display = "none";
+  countAttempts.style.display = "none";
 
   gameAudio.play();
   ctx.drawImage(bg, 0, 0);
@@ -180,6 +205,27 @@ function draw() {
       });
     }
 
+    if (score >= 2) {
+      attempts += 1;
+      countAttempts.innerHTML = `Count of attempts: ${attempts.toString()}`;
+      countAttempts.style.display = "block";
+      ctx.drawImage(bg, 0, 0);
+      stopTimer();
+      gameAudio.pause();
+      gameOver = true;
+      winAudio.play();
+      restartButton.innerHTML = "Start New Game";
+      restartButton.style.display = "block";
+      restartButton.style.top = "20%";
+      gameOverText.innerHTML = "YOU WIN!";
+      gameOverText.style.animation = "MyAnimationBg .2s ease-in-out infinite";
+      gameOverText.style.display = "block";
+      timerDiv.style.display = "block";
+      
+      ctx.fillText();
+      
+    }
+
     if (
       (xPos + bird.width >= pipe[i].x &&
         xPos <= pipe[i].x + pipeUp.width &&
@@ -189,22 +235,27 @@ function draw() {
     ) {
       ctx.drawImage(bg, 0, 0);
       gameAudio.pause();
-      
+
       gameOver = true;
       restartButton.style.display = "block";
       listScore.style.display = "block";
       yourScore.style.display = "block";
       gameOverText.style.display = "block";
-      
+      gameOverText.style.animation = "none";
+      restartButton.style.top = "20%";
+      timerDiv.style.display = "none";
+      countAttempts.style.display = "none";
+
+      failAudio.play();
+      gameOverText.innerHTML = "GAME OVER!";
+
       scoreControl(score);
 
-      if(score >= 2) {
-        winAudio.play();
-        gameOverText.innerHTML = "YOU WIN!";
-      } else {
-        failAudio.play();
-        gameOverText.innerHTML = "GAME OVER!";
-      }
+      restartButton.innerHTML = "Start Again";
+
+      stopTimer();
+
+      attempts += 1;
 
       return;
     }
@@ -221,10 +272,58 @@ function draw() {
   yPos += grav;
 
   ctx.fillStyle = "#000";
-  ctx.font = "24px Verdana";
+  ctx.font = "16px PressStart2P";
   ctx.fillText("Score: " + score, 10, cvs.height - 20);
 
   requestAnimationFrame(draw);
+}
+//TIMER
+const timerDiv = document.createElement("div");
+timerDiv.className = "timer";
+timerDiv.style.position = "absolute";
+timerDiv.style.top = "55%";
+timerDiv.style.left = "5%";
+timerDiv.style.width = "260px";
+timerDiv.style.display = "block";
+canvasDiv.appendChild(timerDiv);
+
+const timerText = document.createElement("span");
+timerText.textContent = "Game time: ";
+timerText.className = "timer_text";
+const timerDots = document.createElement("span");
+timerDots.textContent = ":";
+timerDots.className = "timer_text";
+
+const minutes = document.createElement("span");
+minutes.textContent = "00";
+minutes.className = "minutes";
+const seconds = document.createElement("span");
+seconds.textContent = "00";
+seconds.className = "seconds";
+
+timerDiv.append(timerText);
+timerDiv.append(minutes);
+timerDiv.append(timerDots);
+timerDiv.append(seconds);
+
+let timer = 0;
+let timerInterval;
+
+function startTimer() {
+  stopTimer();
+  timerInterval = setInterval(() => {
+    timer += 1 / 60;
+    let minutesVal = Math.floor(timer / 60);
+    let secondsVal = Math.floor(timer) - Math.floor(timer / 60) * 60;
+    minutes.innerHTML =
+      minutesVal < 10 ? "0" + minutesVal.toString() : minutesVal;
+    seconds.innerHTML =
+      secondsVal < 10 ? "0" + secondsVal.toString() : secondsVal;
+  }, 10);
+}
+
+function stopTimer() {
+  clearInterval(timerInterval);
 }
 
 pipeBottom.onload = startGame;
